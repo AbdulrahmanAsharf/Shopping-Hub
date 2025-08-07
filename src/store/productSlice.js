14a9aca31064c41b18098c3d98798e03f6696bec
Bode Ashraf <bodeasharf52@gmail.com>
2025-08-07 07:04:20 +0300

    fix: move BASE_URL to utils/apiURL

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -20,18 +20,18 @@ const productSlice = createSlice({
     },
 });
 
-export const {setProducts, setStatus} = productSlice.actions;
+export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
 export const fetchProducts = () => {
     return async function fetchProductThunk(dispatch){
         dispatch(setStatus(STATUS.LOADING));
-        try{
+        try {
             const response = await fetch(`${BASE_URL}products`);
             const data = await response.json();
             dispatch(setProducts(data));
             dispatch(setStatus(STATUS.IDLE));
-        } catch(error){
+        } catch(error) {
             dispatch(setStatus(STATUS.ERROR));
         }
     }
--- src/utils/apiURl.js
+++ src/utils/apiURl.js
@@ -1,1 +1,3 @@
-export const BASE_URL = `https://api.escuelajs.co/api/v1/`;
+// src/utils/apiURL.js
+
+export const BASE_URL = 'https://api.escuelajs.co/api/v1/';
